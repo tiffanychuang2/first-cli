09671a38c01efd5a37276658d2f3e11c9292b561
tiffanychuang2 <tiffany.chuang2@gmail.com>
2021-03-31 22:34:23 -0400

    add website

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-#!/usr/bin/env node
+// https://dev.to/dendekky/how-to-build-a-command-line-tool-with-nodejs-a-step-by-step-guide-386k
 
+#!/usr/bin/env node
 console.log("Hello, here is my first CLI tool");
